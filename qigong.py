23bcf6928485651c986ff4c55ad0dc2357193709
Stella <stellachavesyoga@gmail.com>
2018-06-04 15:03:11 -0700

    Create qigong.py

---
+++ qigong.py
@@ -0,0 +1,46 @@
+import time
+import webbrowser
+
+break1 = 1
+break2 = 2
+break3 = 3
+break4 = 4
+break5 = 5
+break6 = 6
+break7 = 7
+break8 = 8
+count = 0
+
+print ("this programe starts on"+time.ctime())
+while (count < break1):
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=c1KKDus6zlE")
+    count = count + 1
+while (count < break2):
+    time.sleep(60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=Xh0ZLeQNfrM")
+    count = count + 1
+while (count < break3):
+    time.sleep(60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=x6JTVm6Lw9c")
+    count = count + 1
+while (count < break4):
+    time.sleep(60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=0tE4NJVX07Y")
+    count = count + 1
+while (count < break5):
+    time.sleep(60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=o9kvYnWKG3g")
+    count = count + 1
+while (count < break6):
+    time.sleep(60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=o2UGXJ1I0CQ&t=16s")
+    count = count + 1
+while (count < break7):
+    time.sleep(60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=yIrNiv16zRo&t=28s")
+    count = count + 1
+while (count < break8):
+    time.sleep(60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=6b33BdOLXYA&t=14s")
+    count = count + 1
